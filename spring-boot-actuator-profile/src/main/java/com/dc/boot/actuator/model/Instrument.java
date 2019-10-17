2dac0b72daf5238c7e71a4dfdaf0318ecd1f4951
anwaaralikhan <manwaaronline@gmail.com>
2019-10-17 19:54:22 -0400

    Added Instrument Information

--- spring-boot-actuator-profile/src/main/java/com/dc/boot/actuator/model/Instrument.java
+++ spring-boot-actuator-profile/src/main/java/com/dc/boot/actuator/model/Instrument.java
@@ -1,5 +1,75 @@
 package com.dc.boot.actuator.model;
 
-public class Instrument {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicLong;
 
+public class Instrument {
+	
+	private String methodName;
+	private AtomicLong count;
+	private AtomicLong min;
+	private AtomicLong max;
+	private AtomicLong totalTime;
+	private List<Long> timeStore = new ArrayList<>();
+	private AtomicLong average;
+	private AtomicLong median;
+	private AtomicLong standardDeviation;
+	
+	public AtomicLong getCount() {
+		return count;
+	}
+	public void setCount(AtomicLong count) {
+		this.count = count;
+	}
+	public AtomicLong getMin() {
+		return min;
+	}
+	public void setMin(AtomicLong min) {
+		this.min = min;
+	}
+	public AtomicLong getMax() {
+		return max;
+	}
+	public void setMax(AtomicLong max) {
+		this.max = max;
+	}
+	public AtomicLong getTotalTime() {
+		return totalTime;
+	}
+	public void setTotalTime(AtomicLong totalTime) {
+		this.totalTime = totalTime;
+	}
+	public List<Long> getTimeStore() {
+		return timeStore;
+	}
+	public void setTimeStore(List<Long> timeStore) {
+		this.timeStore = timeStore;
+	}
+	public AtomicLong getAverage() {
+		return average;
+	}
+	public void setAverage(AtomicLong average) {
+		this.average = average;
+	}
+	public AtomicLong getMedian() {
+		return median;
+	}
+	public void setMedian(AtomicLong median) {
+		this.median = median;
+	}
+	public AtomicLong getStandardDeviation() {
+		return standardDeviation;
+	}
+	public void setStandardDeviation(AtomicLong standardDeviation) {
+		this.standardDeviation = standardDeviation;
+	}
+	public String getMethodName() {
+		return methodName;
+	}
+	public void setMethodName(String methodName) {
+		this.methodName = methodName;
+	}
+	
+	
 }
---
+++ spring-boot-actuator-profile/src/main/java/com/dc/boot/actuator/store/InstrumentStore.java
@@ -0,0 +1,8 @@
+package com.dc.boot.actuator.store;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class InstrumentStore {
+
+}
