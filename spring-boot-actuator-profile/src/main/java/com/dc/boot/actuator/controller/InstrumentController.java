66daa128075f59584a8b408cca7408c75e8eff09
anwaaralikhan <manwaaronline@gmail.com>
2019-10-17 20:19:46 -0400

    Spring AOP Dependancy
    1- Aspects
    2- Code For Instrumentation Added

--- spring-boot-actuator-profile/src/main/java/com/dc/boot/actuator/controller/InstrumentController.java
+++ spring-boot-actuator-profile/src/main/java/com/dc/boot/actuator/controller/InstrumentController.java
@@ -1,5 +1,30 @@
 package com.dc.boot.actuator.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.dc.boot.actuator.model.Instrument;
+
+@RestController
+@RequestMapping("/instrument")
 public class InstrumentController {
 
+	@GetMapping()
+	public String randomUUID() {
+		return UUID.randomUUID().toString();
+	}
+	
+	@GetMapping("/details")
+	public void getInstrumentDetails() {
+		List<Instrument> instruments = new ArrayList<>();
+		
+		Instrument instance = new Instrument();
+		
+		
+	}
 }
