2caf3942686cd069ace91207fdf0ce6fd799e471
anwaaralikhan <manwaaronline@gmail.com>
2019-10-18 20:05:33 -0400

    Controller added with Instrument logic

--- spring-boot-actuator-profile/src/main/java/com/dc/boot/actuator/controller/InstrumentController.java
+++ spring-boot-actuator-profile/src/main/java/com/dc/boot/actuator/controller/InstrumentController.java
@@ -8,23 +8,34 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.dc.boot.actuator.annotations.EnableInstrumentation;
 import com.dc.boot.actuator.model.Instrument;
 
 @RestController
 @RequestMapping("/instrument")
 public class InstrumentController {
 
+	
 	@GetMapping()
+	@EnableInstrumentation
 	public String randomUUID() {
 		return UUID.randomUUID().toString();
 	}
 	
 	@GetMapping("/details")
-	public void getInstrumentDetails() {
+	@EnableInstrumentation
+	public List<Instrument> getInstrumentDetails() {
 		List<Instrument> instruments = new ArrayList<>();
-		
-		Instrument instance = new Instrument();
-		
-		
+		instruments.add(new Instrument("Test1", 5L, 5L, 5L, 5L, 5L, 5L, 5L));
+		instruments.add(new Instrument("Test2", 5L, 5L, 5L, 5L, 5L, 5L, 5L));
+		instruments.add(new Instrument("Test3", 5L, 5L, 5L, 5L, 5L, 5L, 5L));
+		instruments.add(new Instrument("Test4", 5L, 5L, 5L, 5L, 5L, 5L, 5L));
+		instruments.add(new Instrument("Test5", 5L, 5L, 5L, 5L, 5L, 5L, 5L));
+		instruments.add(new Instrument("Test6", 5L, 5L, 5L, 5L, 5L, 5L, 5L));
+		instruments.add(new Instrument("Test7", 5L, 5L, 5L, 5L, 5L, 5L, 5L));
+		instruments.add(new Instrument("Test8", 5L, 5L, 5L, 5L, 5L, 5L, 5L));
+		instruments.add(new Instrument("Test9", 5L, 5L, 5L, 5L, 5L, 5L, 5L));
+		instruments.add(new Instrument("Test10", 5L, 5L, 5L, 5L, 5L, 5L, 5L));
+		return instruments;
 	}
 }
